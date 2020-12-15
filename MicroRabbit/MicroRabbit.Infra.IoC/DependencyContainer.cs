d451e8047acc9442e16ae039abb6b4ea9cbb3931
Nika Kurdadze <nika.kurdadze@ens.tsu.edu.ge>
2020-12-15 17:49:54 +0400

    added dependencies to ioc

--- MicroRabbit/MicroRabbit.Infra.IoC/DependencyContainer.cs
+++ MicroRabbit/MicroRabbit.Infra.IoC/DependencyContainer.cs
@@ -1,6 +1,11 @@
-﻿using MicroRabbit.Domain.Core.Bus;
+﻿using MicroRabbit.Banking.Application.Intefaces;
+using MicroRabbit.Banking.Application.Services;
+using MicroRabbit.Banking.Domain.Interfaces;
+using MicroRabbit.Domain.Core.Bus;
 using MicroRabbit.Infra.Bus;
 using Microsoft.Extensions.DependencyInjection;
+using MiroRabbit.Banking.Data.Context;
+using MiroRabbit.Banking.Data.Repositries;
 
 namespace MicroRabbit.Infra.IoC
 {
@@ -9,6 +14,13 @@ namespace MicroRabbit.Infra.IoC
         public void RegisterServices(IServiceCollection services)
         {
             services.AddTransient<IEventBus, RabbitMQBus>();
+
+            //Services
+            services.AddTransient<IAccountService, AccountService>();
+
+            //Repositories
+            services.AddTransient<IAccountRepository, AccountRepository>();
+            services.AddTransient<BankingContext>();
         }
     }
 }
