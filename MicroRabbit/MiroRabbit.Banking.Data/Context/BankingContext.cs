1dbf8ff80c734e25fe8be6ba2eca63a71fbf8b1e
Nika Kurdadze <nika.kurdadze@ens.tsu.edu.ge>
2020-12-15 17:47:16 +0400

    added accountservice and accountrepository + ef core

---
+++ MicroRabbit/MiroRabbit.Banking.Data/Context/BankingContext.cs
@@ -0,0 +1,14 @@
+﻿using MicroRabbit.Banking.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MiroRabbit.Banking.Data.Context
+{
+    public class BankingContext : DbContext
+    {
+        public BankingContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Account> Accounts{ get; set; }
+    }
+}
---
+++ MicroRabbit/MiroRabbit.Banking.Data/Repositries/AccountRepository.cs
@@ -0,0 +1,22 @@
+﻿using MicroRabbit.Banking.Domain.Interfaces;
+using MicroRabbit.Banking.Domain.Models;
+using MiroRabbit.Banking.Data.Context;
+using System;
+using System.Collections.Generic;
+
+namespace MiroRabbit.Banking.Data.Repositries
+{
+    public class AccountRepository : IAccountRepository
+    {
+        private readonly BankingContext _context;
+
+        public AccountRepository(BankingContext context)
+        {
+            _context = context;
+        }
+        public IEnumerable<Account> GetAccounts()
+        {
+            return _context.Accounts;
+        }
+    }
+}
